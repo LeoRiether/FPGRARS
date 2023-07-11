4b20b4865c12a1b8288c18903ae93346bcdcc26b
LeoRiether <leonardoalves8217@gmail.com>
2023-07-11 14:52:56 -0300

    Added riscv-tests from RARS
    https://github.com/TheThirdOne/rars/blob/a529a70b5cd1bd699f98f5749d34a9eba8232890/test/riscv-tests/
    
    RARS's tests are copied from the riscv-tests repository though afaik

---
+++ tests/riscv-tests/readme.txt
@@ -0,0 +1,1 @@
+The tests in this directory were generated and modified from the riscv-tests and TheThirdOne/rars repositories. They cover basic conformance of instructions to the specification.
---
+++ tests/riscv_tests.rs
@@ -0,0 +1,36 @@
+use std::{path::Path, process::Command};
+
+fn run(path: &impl AsRef<Path>) {
+    let fpgrars = Command::new("cargo")
+        .args(["run", "--release", "--"])
+        .arg(path.as_ref())
+        .status();
+
+    assert!(fpgrars.is_ok(), "Failed to run FPGRARS!");
+    let status = fpgrars.unwrap();
+
+    assert_eq!(
+        status.code(),
+        Some(42),
+        "FPGRARS returned status {status} in test <{}>!",
+        path.as_ref().display()
+    );
+}
+
+#[test]
+fn test_riscv() {
+    let dir = Path::new("tests/riscv-tests");
+    assert!(dir.is_dir(), "riscv-tests directory not found!");
+
+    for file in dir.read_dir().unwrap() {
+        assert!(file.is_ok());
+        let file = file.unwrap();
+
+        if file.file_type().unwrap().is_file() {
+            let path = file.path();
+            if path.extension().unwrap() == "s" {
+                run(&path);
+            }
+        }
+    }
+}
